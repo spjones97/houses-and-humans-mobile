038a7b9f69807f3054f30541aba8288860f82480
Spencer <sjones97@email.unc.edu>
2022-12-01 17:16:48 -0500

    add folder structure, logo, and title, need to clean up

--- client/App.js
+++ client/App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { Appbar, Card } from 'react-native-paper';
+
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app</Text>
-      <StatusBar style="auto" />
+      <Appbar>
+        <Text variant="titleMedium">Items</Text>
+        <Image
+          source={require('./assets/logo.png')}
+        />
+      </Appbar>
     </View>
   );
 }
