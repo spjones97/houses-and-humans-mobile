763be0d1821ccce5a8e065ff1a66f1205a871573
Spencer <sjones97@email.unc.edu>
2022-12-19 12:30:02 -0500

    setup WelcomeScreen and folder structure

---
+++ client/app/screens/WelcomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+function WelcomeScreen(props) {
+  return <View></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default WelcomeScreen;
